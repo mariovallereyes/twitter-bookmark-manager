518856c1e711de3cc14d7851a0513f94c5818b76
Your Name <your.email@example.com>
2025-03-13 08:21:52 -0700

    Fix to user_context_final.py after changes to api main script

--- twitter_bookmark_manager/deployment/final/auth/user_context_final.py
+++ twitter_bookmark_manager/deployment/final/auth/user_context_final.py
@@ -40,7 +40,7 @@ def login_required(f):
     @functools.wraps(f)
     def decorated_function(*args, **kwargs):
         if not UserContext.is_authenticated():
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('auth.login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
